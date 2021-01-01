2c1b08d9d7916f547bf06a391270687bd74505e3
Anthony Cervantes <anthony@cleartech-sa.com>
2020-12-31 21:12:27 -0600

    init

--- index.js
+++ index.js
@@ -10,16 +10,28 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
+  if (msg.mentions.users.size > 0) {
+    if (msg.mentions.users.first().id == bot.user.id) {
+      msg.channel.send("message received.")
+      console.log(msg.content);
+      if (msg.content === "ping") {
+        msg.channel.send("pong");
+      }
     }
+    console.log(msg.mentions.users.first().id);
+    console.log(bot.user.id);
   }
+  
+  // if (msg.content === 'ping') {
+  //   msg.reply('pong');
+  //   msg.channel.send('pong');
+
+  // } else if (msg.content.startsWith('!kick')) {
+  //   if (msg.mentions.users.size) {
+  //     const taggedUser = msg.mentions.users.first();
+  //     msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  //   } else {
+  //     msg.reply('Please tag a valid user!');
+  //   }
+  // }
 });
